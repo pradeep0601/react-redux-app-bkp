c8fee16f7b5ce7281205d06994d236cfd5a0ca15
Pradeep Kumar <pradeku7@cisco.com>
2019-02-28 00:02:41 +0530

    add comment form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import CommentList from "./components/CommentList";
 import CommentsData from './data/CommentsData';
+import CommentForm from './components/CommentForm';
 
 const App = () => (
 <div className="App">
+      <div className="comment-box">
       <CommentList comments={CommentsData.comments}/>
+      <CommentForm />
+      </div>
     </div>
 )
 export default App;
